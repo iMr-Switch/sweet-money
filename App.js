0726bb8e7d38e1574cb3f0b7c4b15437d7b5e788
antonio-colmakers <antonio@colmakers.com>
2018-05-11 10:48:09 -0500

    Add fab button

--- App.js
+++ App.js
@@ -11,6 +11,8 @@ import {
   Text,
   View
 } from 'react-native';
+import ActionButton from 'react-native-action-button';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
@@ -23,16 +25,32 @@ type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
+      <View style={{flex:1, backgroundColor: '#f3f3f3'}}>
+      <Text style={styles.welcome}>
          Sweet Money
-        </Text>        
-      </View>
+        </Text>  
+      <ActionButton buttonColor="rgba(231,76,60,1)">
+        <ActionButton.Item buttonColor='#9b59b6' title="New Task" onPress={() => console.log("notes tapped!")}>
+          <Icon name="md-create" style={styles.actionButtonIcon} />
+        </ActionButton.Item>
+        <ActionButton.Item buttonColor='#3498db' title="Notifications" onPress={() => {}}>
+          <Icon name="md-notifications-off" style={styles.actionButtonIcon} />
+        </ActionButton.Item>
+        <ActionButton.Item buttonColor='#1abc9c' title="All Tasks" onPress={() => {}}>
+          <Icon name="md-done-all" style={styles.actionButtonIcon} />
+        </ActionButton.Item>
+      </ActionButton>
+    </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  actionButtonIcon: {
+    fontSize: 20,
+    height: 22,
+    color: 'white',
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
