d25bd756fe352429e015054f3d922145e059d505
antonio-colmakers <antonio@colmakers.com>
2018-06-01 11:00:47 -0500

    Add icons

--- App.js
+++ App.js
@@ -30,15 +30,12 @@ export default class App extends Component<Props> {
          Sweet Money
         </Text>  
       <ActionButton buttonColor="rgba(231,76,60,1)">
-        <ActionButton.Item buttonColor='#9b59b6' title="New Task" onPress={() => console.log("notes tapped!")}>
+        <ActionButton.Item buttonColor='#9b59b6' title="Agregar Ingreso" onPress={() => console.log("notes tapped!")}>
           <Icon name="md-create" style={styles.actionButtonIcon} />
         </ActionButton.Item>
-        <ActionButton.Item buttonColor='#3498db' title="Notifications" onPress={() => {}}>
+        <ActionButton.Item buttonColor='#3498db' title="Agregar Gasto" onPress={() => {}}>
           <Icon name="md-notifications-off" style={styles.actionButtonIcon} />
-        </ActionButton.Item>
-        <ActionButton.Item buttonColor='#1abc9c' title="All Tasks" onPress={() => {}}>
-          <Icon name="md-done-all" style={styles.actionButtonIcon} />
-        </ActionButton.Item>
+        </ActionButton.Item>        
       </ActionButton>
     </View>
     );
