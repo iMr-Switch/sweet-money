d0b9a9244f9d6da38fd9e563080b61687323b3d8
antonio-colmakers <antonio@colmakers.com>
2018-06-08 10:41:08 -0500

    Re install app

--- ios/SweetMoneyTests/SweetMoneyTests.m
+++ ios/SweetMoneyTests/SweetMoneyTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface sweetMoneyTests : XCTestCase
+@interface SweetMoneyTests : XCTestCase
 
 @end
 
-@implementation sweetMoneyTests
+@implementation SweetMoneyTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
